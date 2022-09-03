aa46c3e4c55016d390d4a3eb539f6317800422b5
NAbdullokh <country.abu@gmail.com>
2022-09-03 21:24:49 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,176 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import './App.css'
+import "./App.css";
 
 const App = () => {
-  const [preState, setPreState] = useState('')
-  const [curState, setCurState] = useState('')
-  const [input, setInput] = useState('0')
-  const [operator, setOperator] = useState(null)
-  const [total, setTotal] = useState(false)
+  const [preState, setPreState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
 
-  const inputNum = e => {
-    if (curState.includes('.') && e.target.innerText === '.') return 
+  const inputNum = (e) => {
+    if (curState.includes(".") && e.target.innerText === ".") return;
 
     if (total) {
-      setPreState('')
+      setPreState("");
     }
 
-    curState ? setCurState(pre => pre + e.target.innerText) : setCurState(e.target.innerText)
-    setTotal(false)
-
-  }
-
-  useEffect(() => {
-    setInput(curState)
-  }, [curState])
+    curState
+      ? setCurState((pre) => pre + e.target.innerText)
+      : setCurState(e.target.innerText);
+    setTotal(false);
+  };
 
   useEffect(() => {
-    setInput('0')
-  },[])
+    setInput(curState);
+  }, [curState]);
 
-  const operatorType = e => {
-    
-  }
+  useEffect(() => {
+    setInput("0");
+  }, []);
 
-  const equals = e => {
-    
-  }
+  const operatorType = (e) => {
+    setTotal(false);
+    setOperator(e.target.innerText);
+    if (curState === "") return;
+    if (preState !== "") {
+      equals();
+    }
+    setPreState(curState);
+    setCurState("");
+  };
 
-  const minusPlus = e => {
-    
-  }
+  const equals = (e) => {
+    if (e?.target.innerText === "=") setTotal(true);
 
-  const reset = e => {
-    
-  }
+    let cal;
+    switch (operator) {
+      case "/":
+        cal = String(parseFloat(preState) / parseFloat(curState));
+        break;
 
-  const persent = e => {
-    
-  }
+      case "*":
+        cal = String(parseFloat(preState) * parseFloat(curState));
+        break;
+
+      case "+":
+        cal = String(parseFloat(preState) + parseFloat(curState));
+        break;
+
+      case "-":
+        cal = String(parseFloat(preState) - parseFloat(curState));
+        break;
+
+      default:
+        return;
+    }
+    setInput("");
+    setPreState(cal);
+    setCurState("");
+  };
+  const minusPlus = (e) => {
+    if (curState.charAt(0) === "-") {
+      setCurState(curState.substring(1));
+    } else {
+      setCurState("-" + curState);
+    }
+  };
+
+  const reset = (e) => {
+    setPreState("");
+    setCurState("");
+    setInput("0");
+  };
+
+  const persent = (e) => {
+    preState
+      ? setCurState(String((parseFloat(curState) / 100) * preState))
+      : setCurState(String(parseFloat(curState) / 100));
+  };
 
   return (
     <div className="container">
       <div className="wrapper">
-        <div className="screen">{input}</div>
-        <div className="btn light-gray" onClick={reset}>AC</div>
-        <div className="btn light-gray" onClick={persent}>%</div>
-        <div className="btn light-gray" onClick={minusPlus}>+/-</div>
-        <div className="btn orange" onClick={operatorType}>/</div>
-        <div className="btn" onClick={inputNum}>7</div>
-        <div className="btn" onClick={inputNum}>8</div>
-        <div className="btn" onClick={inputNum}>9</div>
-        <div className="btn orange" onClick={operatorType}>*</div>
-        <div className="btn" onClick={inputNum}>4</div>
-        <div className="btn" onClick={inputNum}>5</div>
-        <div className="btn" onClick={inputNum}>6</div>
-        <div className="btn orange" onClick={operatorType}>+</div>
-        <div className="btn" onClick={inputNum}>1</div>
-        <div className="btn" onClick={inputNum}>2</div>
-        <div className="btn" onClick={inputNum}>3</div>
-        <div className="btn orange" onClick={operatorType}>-</div>
-        <div className="btn" onClick={inputNum}>0</div>
-        <div className="btn" onClick={inputNum}>.</div>
-        <div className="btn" onClick={equals}>=</div>
+        <div className="screen">
+          {input !== "" || input === "0" ? (
+            <NumberFormat
+              value={input}
+              displayType={"text"}
+              thousandSepartor={true}
+            />
+          ) : (
+            <NumberFormat
+              value={preState}
+              displayType={"text"}
+              thousandSepartor={true}
+            />
+          )}{" "}
+        </div>
+        <div className="btn light-gray" onClick={reset}>
+          AC
+        </div>
+        <div className="btn light-gray" onClick={persent}>
+          %
+        </div>
+        <div className="btn light-gray" onClick={minusPlus}>
+          +/-
+        </div>
+        <div className="btn orange" onClick={operatorType}>
+          /
+        </div>
+        <div className="btn" onClick={inputNum}>
+          7
+        </div>
+        <div className="btn" onClick={inputNum}>
+          8
+        </div>
+        <div className="btn" onClick={inputNum}>
+          9
+        </div>
+        <div className="btn orange" onClick={operatorType}>
+          *
+        </div>
+        <div className="btn" onClick={inputNum}>
+          4
+        </div>
+        <div className="btn" onClick={inputNum}>
+          5
+        </div>
+        <div className="btn" onClick={inputNum}>
+          6
+        </div>
+        <div className="btn orange" onClick={operatorType}>
+          +
+        </div>
+        <div className="btn" onClick={inputNum}>
+          1
+        </div>
+        <div className="btn" onClick={inputNum}>
+          2
+        </div>
+        <div className="btn" onClick={inputNum}>
+          3
+        </div>
+        <div className="btn orange" onClick={operatorType}>
+          -
+        </div>
+        <div className="btn zero" onClick={inputNum}>
+          0
+        </div>
+        <div className="btn" onClick={inputNum}>
+          .
+        </div>
+        <div className="btn" onClick={equals}>
+          =
+        </div>
       </div>
     </div>
   );
 };
 
 export default App;
+
+
+// npm i react-number-format
